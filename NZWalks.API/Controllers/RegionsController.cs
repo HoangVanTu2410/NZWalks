b1d995c161c5ab7a4c2c82c62e7811e28800c417
Hoang Tu <Hoang Tu@DESKTOP-PN02UCH>
2024-12-31 16:39:02 +0700

    Add project files.

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+        public RegionsController(NZWalksDbContext dbContext) 
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var regionsDomain = await dbContext.Regions.ToListAsync();
+
+            var regionsDTO = regionsDomain.Select(item => new RegionDTO
+            {
+                Id = item.Id,
+                Code = item.Code,
+                Name = item.Name,
+                RegionImageUrl = item.RegionImageUrl,
+            });
+
+            return Ok(regionsDTO);
+        }
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetById([FromRoute] Guid id)
+        {
+            var regionDomain = await dbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (regionDomain == null)
+            {
+                return NotFound();
+            }
+
+            var regionDTO = new RegionDTO
+            {
+                Id = regionDomain.Id,
+                Code = regionDomain.Code,
+                Name = regionDomain.Name,
+                RegionImageUrl = regionDomain.RegionImageUrl
+            };
+
+            return Ok(regionDTO);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] AddRegionDTO addRegionDTO)
+        {
+            var region = new Region
+            {
+                Code = addRegionDTO.Code,
+                Name = addRegionDTO.Name,
+                RegionImageUrl = addRegionDTO.RegionImageUrl
+            };
+
+            await dbContext.Regions.AddAsync(region);
+            await dbContext.SaveChangesAsync();
+
+            var regionDTO = new RegionDTO
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl
+            };
+
+            return CreatedAtAction(nameof(GetById), new { id = regionDTO.Id }, regionDTO);
+        }
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateRegionDTO updateRegionDTO)
+        {
+            var region = await dbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (region == null) {
+                return NotFound();
+            }
+
+            region.Code = updateRegionDTO.Code;
+            region.Name = updateRegionDTO.Name;
+            region.RegionImageUrl = updateRegionDTO.RegionImageUrl;
+
+            await dbContext.SaveChangesAsync();
+
+            var regionDTO = new RegionDTO
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl
+            };
+
+            return Ok(regionDTO);
+        }
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
+        {
+            var region = await dbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            dbContext.Remove(region);
+            await dbContext.SaveChangesAsync();
+
+            var regionDTO = new RegionDTO
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl
+            };
+
+            return Ok(regionDTO);
+        }
+    }
+}
---
+++ NZWalks.API/Controllers/WalksController.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalksController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+
+        public WalksController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var walksDomain = await dbContext.Walks.Include("Difficulty").Include("Region").ToListAsync();
+
+            var walksDTO = walksDomain.Select(item => new WalkDTO
+            {
+                Id = item.Id,
+                Name = item.Name,
+                Description = item.Description,
+                LengthInKm = item.LengthInKm,
+                WalkImageUrl = item.WalkImageUrl,
+                Difficulty = item.Difficulty,
+                Region = item.Region
+            });
+
+            return Ok(walksDTO);
+        }
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetById([FromRoute] Guid id)
+        {
+            var walkDomain = await dbContext.Walks.Include("Difficulty").Include("Region").FirstOrDefaultAsync(x => x.Id == id);
+
+            if (walkDomain == null)
+            {
+                return NotFound();
+            }
+
+            var walkDTO = new WalkDTO
+            {
+                Id = walkDomain.Id,
+                Name = walkDomain.Name,
+                Description = walkDomain.Description,
+                LengthInKm = walkDomain.LengthInKm,
+                WalkImageUrl = walkDomain.WalkImageUrl,
+                Difficulty = walkDomain.Difficulty,
+                Region = walkDomain.Region
+            };
+
+            return Ok(walkDTO);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] AddWalkDTO addWalkDTO)
+        {
+            var walkDomain = new Walk
+            {
+                Name = addWalkDTO.Name,
+                Description = addWalkDTO.Description,
+                LengthInKm = addWalkDTO.LengthInKm,
+                WalkImageUrl = addWalkDTO.WalkImageUrl,
+                DifficultyId = addWalkDTO.DifficultyId,
+                RegionId = addWalkDTO.RegionId
+            };
+
+            await dbContext.Walks.AddAsync(walkDomain);
+            await dbContext.SaveChangesAsync();
+
+            var walkDTO = new WalkDTO
+            {
+                Id = walkDomain.Id,
+                Name = walkDomain.Name,
+                Description = walkDomain.Description,
+                LengthInKm = walkDomain.LengthInKm,
+                WalkImageUrl = walkDomain.WalkImageUrl
+            };
+
+            return CreatedAtAction(nameof(GetById), new { id = walkDTO.Id }, walkDTO);
+        }
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateWalkDTO updateWalkDTO)
+        {
+            var walkDomain = await dbContext.Walks.Include("Difficulty").Include("Region").FirstOrDefaultAsync(x => x.Id == id);
+
+            if (walkDomain == null)
+            {
+                return NotFound();
+            }
+
+            walkDomain.Name = updateWalkDTO.Name;
+            walkDomain.Description = updateWalkDTO.Description;
+            walkDomain.LengthInKm = updateWalkDTO.LengthInKm;
+            walkDomain.WalkImageUrl = updateWalkDTO.WalkImageUrl;
+            walkDomain.DifficultyId = updateWalkDTO.DifficultyId;
+            walkDomain.RegionId = updateWalkDTO.RegionId;
+
+            await dbContext.SaveChangesAsync();
+
+            var walkDTO = new WalkDTO
+            {
+                Id = walkDomain.Id,
+                Name = walkDomain.Name,
+                Description = walkDomain.Description,
+                LengthInKm = walkDomain.LengthInKm,
+                WalkImageUrl = walkDomain.WalkImageUrl,
+                Difficulty = walkDomain.Difficulty,
+                Region = walkDomain.Region
+            };
+
+            return Ok(walkDTO);
+        }
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
+        {
+            var walkDomain = await dbContext.Walks.Include("Difficulty").Include("Region").FirstOrDefaultAsync(x => x.Id == id);
+
+            if (walkDomain == null)
+            {
+                return NotFound();
+            }
+
+            dbContext.Walks.Remove(walkDomain);
+            await dbContext.SaveChangesAsync();
+
+            var walkDTO = new WalkDTO
+            {
+                Id = walkDomain.Id,
+                Name = walkDomain.Name,
+                Description = walkDomain.Description,
+                LengthInKm = walkDomain.LengthInKm,
+                WalkImageUrl = walkDomain.WalkImageUrl,
+                Difficulty = walkDomain.Difficulty,
+                Region = walkDomain.Region
+            };
+
+            return Ok(walkDTO);
+        }
+    }
+}
